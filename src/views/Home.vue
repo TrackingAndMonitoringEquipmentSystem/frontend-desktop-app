dd07cb36e7abad444d6e48b66429b7a9a94ed6b1
godprogrammer3 <game47you@gmail.com>
2022-04-17 02:45:59 +0700

    finished received update locker

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,41 @@
   <v-container fill-height>
     <v-card width="100%" height="100%">
       <v-row justify="center" class="fill-height">
-        <v-col class="mt-16">
+        <v-col class="mt-16 display-1">
           <div v-if="isUnregister">
-            <v-row justify="center"
-              ><span>สถานะ : </span><span> ยังไม่ได้ลงทะเบียน</span></v-row
+            <v-row justify="center" class="ma-5"
+              ><span>สถานะ :&nbsp;</span><span> ยังไม่ได้ลงทะเบียน</span></v-row
             >
-            <v-row justify="center"
-              ><span>LockerId : </span><span>{{ lockerId }}</span></v-row
+            <v-row justify="center" class="ma-5"
+              ><span>LockerId :&nbsp;</span><span>{{ lockerId }}</span></v-row
             >
-            <v-row justify="center">
+            <v-row justify="center" class="ma-5">
               <QRCode :text="lockerId"></QRCode>
             </v-row>
+          </div>
+          <div v-else>
+            <v-col>
+              <v-row>
+                <h5>ข้อมูลรายละเอียด</h5>
+              </v-row>
+              <v-divider></v-divider>
+              <v-row>
+                <v-col>
+                  <span>ชื่อตู้ล็อกเกอร์ :</span>
+                </v-col>
+                <v-col>
+                  <span>{{ name }}</span>
+                </v-col>
+              </v-row>
+              <v-row>
+                <v-col>
+                  <span>คำอธิบาย :</span>
+                </v-col>
+                <v-col>
+                  <span>{{ description }}</span>
+                </v-col>
+              </v-row>
+            </v-col>
           </div>
         </v-col>
       </v-row>
@@ -64,17 +88,18 @@ export default {
     this.socket.on("connect_error", (error) => {
       console.log("connect_error:", error);
     });
-    this.socket.on("locker_update", (lockerData) => {
-      console.log("locker_update:", lockerData);
-      this.lockerName = lockerData.name;
+    this.socket.on("locker_updated", (locker) => {
+      console.log("->locker_updated:", locker);
+      this.locker = locker;
     });
     this.socket.on("faceRecognitionResult", (result) => {
       console.log("->result:", result);
     });
+    this.se;
   },
   computed: {
     name() {
-      return this.lockerName ?? "Not Already Registered";
+      return this.locker?.name ?? "";
     },
     displayToggleFaceRecognitionText() {
       return !this.isRunningFaceRecognition ? "start" : "stop";
@@ -84,6 +109,9 @@ export default {
     },
     lockerId() {
       return this.locker?.id.toString() ?? "";
+    },
+    description() {
+      return this.locker?.description ?? "";
     },
   },
   methods: {
